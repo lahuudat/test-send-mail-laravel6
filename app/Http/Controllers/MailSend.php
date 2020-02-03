122f404fa638ac16e03ce95a8dd8db8a68515cd7
La Hữu Đạt <datlh@bap.jp>
2020-02-03 10:36:32 +0700

    first commit

---
+++ app/Http/Controllers/MailSend.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\Mail\SendMail;
+
+class MailSend extends Controller
+{
+    public function mailsend()
+    {
+        $details = [
+            'title' => 'Title: Mail from Real Programmer',
+            'body' => 'Body: This is for testing email using smtp'
+        ];
+    
+        \Mail::to('datlh@bap.jp')->send(new SendMail($details));
+        return view('thanks');
+    }
+}
